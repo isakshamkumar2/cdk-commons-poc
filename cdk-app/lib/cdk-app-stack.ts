ebd0ce22330f73e48b4bc46138719cc9aa9d90be
isakshamkumar2 <saksham@genflowly.com>
2024-08-05 16:52:23 +0530

    [REFACTOR] Added step to correctly download wheels file when starting the ec2 container

--- cdk-app/lib/cdk-app-stack.ts
+++ cdk-app/lib/cdk-app-stack.ts
@@ -3,20 +3,16 @@ import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 import * as path from 'path';
 import * as fs from 'fs';
 import { createDefaultVpc, STAGES, createDefaultSecurityGroup, createIAMRole, attachManagedPolicyToRole, MANAGED_POLICIES } from "@isakshamkumar2/cdk-common-test"
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-
-
 
     // VPC creation
     const vpc = createDefaultVpc('MyVpc', 'FlaskAppVPC', 2, this, STAGES.BETA, true);
@@ -31,10 +27,11 @@ export class CdkAppStack extends cdk.Stack {
       this                             
     );
 
-    // Allow inbound traffic on ports 80 and 5000
+    // Allow inbound traffic on ports 80, 5000, and 22
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic');
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(5000), 'Allow Flask app traffic');
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH access');
+
     // IAM role creation
     const role = createIAMRole(
       'EC2Role',                                
@@ -45,48 +42,47 @@ export class CdkAppStack extends cdk.Stack {
     );
     
     attachManagedPolicyToRole(role, MANAGED_POLICIES.SSM_MANAGED_INSTANCE_CORE);
-    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
-role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'));
 
+    // Create S3 bucket for wheel files
+    const wheelsBucket = new s3.Bucket(this, 'WheelsBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // Upload wheel files to S3 bucket
+    new s3deploy.BucketDeployment(this, 'DeployWheels', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '..', '..', 'flask-app', 'wheels'))],
+      destinationBucket: wheelsBucket,
+      destinationKeyPrefix: 'wheels',
+    });
+
+    // Grant read access to the EC2 instances
+    wheelsBucket.grantRead(role);
 
     // Read Flask app contents
     const flaskAppPath = path.join(__dirname, '..', '..', 'flask-app');
     const appPyContent = fs.readFileSync(path.join(flaskAppPath, 'app.py'), 'utf8');
     const wsgiPyContent = fs.readFileSync(path.join(flaskAppPath, 'wsgi.py'), 'utf8');
-    const requirementsContent = fs.readFileSync(path.join(flaskAppPath, 'requirements.txt'), 'utf8');
 
-    const wheelsBucket = new s3.Bucket(this, 'WheelsBucket', {
-      bucketName:'isakshamkumar2-test-bucket',
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    });
-    wheelsBucket.grantRead(role);
-    // Upload wheel files to the S3 bucket
-new s3deploy.BucketDeployment(this, 'DeployWheels', {
-  sources: [s3deploy.Source.asset(path.join(flaskAppPath, 'wheels'))],
-  destinationBucket: wheelsBucket,
-  destinationKeyPrefix: 'wheels',
-});
-    // Read wheel file names
-   
     const userData = ec2.UserData.forLinux();
     userData.addCommands(
       '#!/bin/bash',
       'set -e',
       'sudo yum update -y',
       'sudo yum install -y python3 python3-pip awscli',
-      'mkdir -p /home/ec2-user/flask-app',
+      'mkdir -p /home/ec2-user/flask-app/wheels',
       
       // Create Flask app files
       `echo '${appPyContent.replace(/'/g, "'\\''")}' > /home/ec2-user/flask-app/app.py`,
       `echo '${wsgiPyContent.replace(/'/g, "'\\''")}' > /home/ec2-user/flask-app/wsgi.py`,
-      `echo '${requirementsContent.replace(/'/g, "'\\''")}' > /home/ec2-user/flask-app/requirements.txt`,
+      
+      // Download wheel files from S3
+      `aws s3 cp s3://${wheelsBucket.bucketName}/wheels/ /home/ec2-user/flask-app/wheels/ --recursive`,
       
       'cd /home/ec2-user/flask-app',
       
-      // Install requirements including Gunicorn
-      'pip3 install -r requirements.txt',
-      'pip3 install gunicorn',
+      // Install wheel files
+      'pip3 install wheels/*.whl',
       
       // Start Flask app with Gunicorn
       'nohup gunicorn --workers 3 --bind 0.0.0.0:5000 wsgi:app > flask.log 2>&1 &',
@@ -100,6 +96,7 @@ new s3deploy.BucketDeployment(this, 'DeployWheels', {
       
       'echo "Flask app deployed successfully"'
     );
+
     
     // Auto Scaling Group creation
     const autoScalingGroup = new autoscaling.AutoScalingGroup(this, 'MyASG', {
